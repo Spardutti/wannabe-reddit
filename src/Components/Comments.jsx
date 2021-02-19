4ff776535dd2a66e7725064f33d3870ca9cfccf8
spardutti <luisdamian.sp@gmail.com>
2021-02-19 13:57:06 -0300

    added some queries

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -4,18 +4,9 @@ import "./styles/comments.css"
 const Comments = (props) => {
   return (
     <div className="comment-container">
-          <p>{props.author}</p>
-          <p>{props.msg}</p>
-      <div className="comment-bottom-bar">
-        <div className="upvote">
-          <i class="fas fa-arrow-up"></i>
-        </div>
-        <div className="votes">10</div>
+          <p>Created by: {props.author}</p>
+          <p className="comment-msg">{props.msg}</p>
 
-        <div className="downvote">
-          <i class="fas fa-arrow-down"></i>
-        </div>
-      </div>
     </div>
   );
 };
